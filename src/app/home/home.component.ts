63d6e7db03af1a238d17478004333a6b04fff81e
Eti Hason <etih@bss.com>
2018-12-25 18:17:40 +0200

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger,style,transition,animate, keyframes, query, stagger } from '@angular/animations';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations:[
+   /* trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({opacity: 0}), {optional: true}),
+
+        query(':enter',stagger('300ms',[
+          animate('.6s ease-in',keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset:0}),
+            style({opacity: 0.5, transform: 'translateY(35px)', offset:0.3}),
+            style({opacity: 1, transform: 'translateY(0)', offset:1}),
+        ]))]),{optional: true})
+      ])
+    ])*/
+  ]
+})
+export class HomeComponent implements OnInit {
+  itemCount: number;
+  dynamicText:string = 'my text...';
+  twoBinding: string = 'defult value..';
+  goals =[];
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    this.goals.push(this.twoBinding);
+    this.twoBinding ='';
+    this.itemCount = this.goals.length;
+  }
+
+  removeItem(i){
+    this.goals.splice(i,1);
+  }
+}
